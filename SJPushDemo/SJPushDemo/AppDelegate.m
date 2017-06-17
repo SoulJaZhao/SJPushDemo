d0ec649242a4a09974c13c29e97260267a8f8a9c
SoulJaZhao <superzhaolong@126.com>
2017-06-17 15:00:34 +0800

    添加Category Action

--- SJPushDemo/SJPushDemo/AppDelegate.m
+++ SJPushDemo/SJPushDemo/AppDelegate.m
@@ -8,6 +8,11 @@
 
 #import "AppDelegate.h"
 
+static NSString *const Notification_Action_Follow_ID = @"Notification_Action_Follow_ID";
+static NSString *const Notification_Action_Cancel_ID = @"Notification_Action_Cancel_ID";
+
+static NSString *const Notification_Category_Follow = @"Notification_Category_Follow";
+
 @interface AppDelegate () <UIAlertViewDelegate>
 
 @end
@@ -50,7 +55,27 @@
 
 #pragma mark - 注册APNS
 - (void)registerAPNS {
-    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeAlert | UIUserNotificationTypeSound categories:nil];
+    //Action Category Setting
+    UIMutableUserNotificationAction *actionFollow = [[UIMutableUserNotificationAction alloc] init];
+    actionFollow.identifier = Notification_Action_Follow_ID;
+    actionFollow.activationMode = UIUserNotificationActivationModeBackground;
+    actionFollow.title = @"关注";
+    actionFollow.destructive = false;
+    [actionFollow setAuthenticationRequired:NO];
+    
+    UIMutableUserNotificationAction *actionCancel = [[UIMutableUserNotificationAction alloc] init];
+    actionCancel.identifier = Notification_Action_Cancel_ID;
+    actionFollow.activationMode = UIUserNotificationActivationModeBackground;
+    actionCancel.title = @"取消";
+    actionCancel.destructive = false;
+    [actionCancel setAuthenticationRequired:NO];
+    
+    UIMutableUserNotificationCategory *categoryFollow = [[UIMutableUserNotificationCategory alloc] init];
+    categoryFollow.identifier = Notification_Category_Follow;
+    [categoryFollow setActions:@[actionFollow,actionCancel] forContext:UIUserNotificationActionContextDefault];
+    NSSet *categorys = [NSSet setWithObject:categoryFollow];
+    
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeAlert | UIUserNotificationTypeSound categories:categorys];
     
     [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
 }
@@ -61,13 +86,17 @@
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"请开启推送开关" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
         alertView.delegate = self;
         [alertView show];
+    } else {
+        [application registerForRemoteNotifications];
     }
-    [application registerForRemoteNotifications];
 }
 
 #pragma mark - 接收到消息通知
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
     completionHandler(UIBackgroundFetchResultNewData);
+    if (application.applicationState == UIApplicationStateBackground) {
+        NSLog(@"从后台进来");
+    }
     NSLog(@"userInfo:%@",userInfo);
 }
 
@@ -94,4 +123,12 @@
         }
     }
 }
+
+#pragma mark - 接收远程按钮
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void (^)())completionHandler {
+    if ([identifier isEqualToString:Notification_Action_Follow_ID]) {
+        NSLog(@"关注");
+    }
+}
+
 @end
