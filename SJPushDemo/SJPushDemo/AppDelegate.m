ab47fa58af9b806a65df0e38e361a04fd50f0354
SoulJaZhao <superzhaolong@126.com>
2017-06-18 15:22:27 +0800

    添加iOS10下的通知

--- SJPushDemo/SJPushDemo/AppDelegate.m
+++ SJPushDemo/SJPushDemo/AppDelegate.m
@@ -7,13 +7,14 @@
 //
 
 #import "AppDelegate.h"
+#import <UserNotifications/UserNotifications.h>
 
 static NSString *const Notification_Action_Follow_ID = @"Notification_Action_Follow_ID";
 static NSString *const Notification_Action_Cancel_ID = @"Notification_Action_Cancel_ID";
 
 static NSString *const Notification_Category_Follow = @"Notification_Category_Follow";
 
-@interface AppDelegate () <UIAlertViewDelegate>
+@interface AppDelegate () <UIAlertViewDelegate,UNUserNotificationCenterDelegate>
 
 @end
 
@@ -22,7 +23,63 @@ static NSString *const Notification_Category_Follow = @"Notification_Category_Fo
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [self registerAPNS];
+//    [self registerAPNS];
+    //iOS10以上系统
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 10.0) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            if (granted) {
+                NSLog(@"注册成功");
+                [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+                    NSLog(@"%@",settings);
+                }];
+            } else {
+                NSLog(@"注册失败");
+            }
+        }];
+    }
+    else if ([[UIDevice currentDevice].systemVersion floatValue] >8.0){
+        //iOS8 - iOS10
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge categories:nil]];
+        
+    }
+    else if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0) {
+        //iOS8系统以下
+        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound];
+    }
+    [application registerForRemoteNotifications];
+    
+//    // 1.创建通知内容
+//    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+//    content.title = @"徐不同测试通知";
+//    content.subtitle = @"测试通知";
+//    content.body = @"来自徐不同的简书";
+//    content.badge = @1;
+////    NSError *error = nil;
+////    NSString *path = [[NSBundle mainBundle] pathForResource:@"icon_certification_status1@2x" ofType:@"png"];
+////    // 2.设置通知附件内容
+////    UNNotificationAttachment *att = [UNNotificationAttachment attachmentWithIdentifier:@"att1" URL:[NSURL fileURLWithPath:path] options:nil error:&error];
+////    if (error) {
+////        NSLog(@"attachment error %@", error);
+////    }
+////    content.attachments = @[att];
+////    content.launchImageName = @"icon_certification_status1@2x";
+//    // 2.设置声音
+//    UNNotificationSound *sound = [UNNotificationSound defaultSound];
+//    content.sound = sound;
+//    
+//    // 3.触发模式
+//    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:5 repeats:NO];
+//    
+//    // 4.设置UNNotificationRequest
+//    NSString *requestIdentifer = @"TestRequest";
+//    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:requestIdentifer content:content trigger:trigger];
+//    
+//    //5.把通知加到UNUserNotificationCenter, 到指定触发点会被触发
+//    [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+//        NSLog(@"%@",error);
+//    }];
     return YES;
 }
 
@@ -111,7 +168,7 @@ static NSString *const Notification_Category_Follow = @"Notification_Category_Fo
 
 #pragma mark - 注册失败
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    NSLog(@"error:%@",error);
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
 }
 
 #pragma mark - UIAlertViewDelegate
@@ -131,4 +188,50 @@ static NSString *const Notification_Category_Follow = @"Notification_Category_Fo
     }
 }
 
+// iOS 10收到通知
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
+    NSDictionary * userInfo = notification.request.content.userInfo;
+    UNNotificationRequest *request = notification.request; // 收到推送的请求
+    UNNotificationContent *content = request.content; // 收到推送的消息内容
+    NSNumber *badge = content.badge;  // 推送消息的角标
+    NSString *body = content.body;    // 推送消息体
+    UNNotificationSound *sound = content.sound;  // 推送消息的声音
+    NSString *subtitle = content.subtitle;  // 推送消息的副标题
+    NSString *title = content.title;  // 推送消息的标题
+    
+    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        NSLog(@"iOS10 前台收到远程通知:%@", userInfo);
+        
+    }
+    else {
+        // 判断为本地通知
+        NSLog(@"iOS10 前台收到本地通知:{\\\\nbody:%@，\\\\ntitle:%@,\\\\nsubtitle:%@,\\\\nbadge：%@，\\\\nsound：%@，\\\\nuserInfo：%@\\\\n}",body,title,subtitle,badge,sound,userInfo);
+    }
+    completionHandler(UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionSound|UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以设置
+}
+
+// 通知的点击事件
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler{
+    
+    NSDictionary * userInfo = response.notification.request.content.userInfo;
+    UNNotificationRequest *request = response.notification.request; // 收到推送的请求
+    UNNotificationContent *content = request.content; // 收到推送的消息内容
+    NSNumber *badge = content.badge;  // 推送消息的角标
+    NSString *body = content.body;    // 推送消息体
+    UNNotificationSound *sound = content.sound;  // 推送消息的声音
+    NSString *subtitle = content.subtitle;  // 推送消息的副标题
+    NSString *title = content.title;  // 推送消息的标题
+    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        NSLog(@"iOS10 收到远程通知:%@", userInfo);
+        
+    }
+    else {
+        // 判断为本地通知
+        NSLog(@"iOS10 收到本地通知:{\\\\nbody:%@，\\\\ntitle:%@,\\\\nsubtitle:%@,\\\\nbadge：%@，\\\\nsound：%@，\\\\nuserInfo：%@\\\\n}",body,title,subtitle,badge,sound,userInfo);
+    }
+    
+    // Warning: UNUserNotificationCenter delegate received call to -userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler: but the completion handler was never called.
+    completionHandler();  // 系统要求执行这个方法
+    
+}
 @end
